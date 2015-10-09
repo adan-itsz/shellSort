4ae9986ab89856a5c5dcc7064447cef7f7dd7c97
Adan De Santiago <adan1995a@gmail.com>
2015-10-08 21:55:19 -0500

    metodo completo

---
+++ ShellSort/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ShellSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxElementos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxElementos
+            // 
+            this.txtBoxElementos.Location = new System.Drawing.Point(12, 101);
+            this.txtBoxElementos.Name = "txtBoxElementos";
+            this.txtBoxElementos.Size = new System.Drawing.Size(131, 22);
+            this.txtBoxElementos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Longitud de arreglo";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(170, 100);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(12, 163);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(151, 29);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Llenar random";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(286, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 323);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Shell Sort";
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.Location = new System.Drawing.Point(12, 226);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(151, 29);
+            this.btnOrdenar.TabIndex = 9;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 368);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxElementos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxElementos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOrdenar;
+    }
+}
+
---
+++ ShellSort/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShellSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arreglo;
+        int longitud;
+        int num;
+        Random rand = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("","");
+        }
+        public int[] shellSort()
+        {
+            bool ban = false;
+            int i;
+            int aux;
+            int acumulador = arreglo.Length / 2 ;//INT
+            while (acumulador > 0)
+            {
+               // acumulador = (acumulador / 2);
+                ban = true;
+                while (ban == true)
+                {
+                    ban = false;
+                    i = 1;
+                    while (i<=(arreglo.Length - acumulador))
+                    {
+                        if (arreglo[i - 1] > arreglo[(i-1) + acumulador])
+                        {
+                            aux = arreglo[i - 1 + acumulador];
+                            arreglo[(i - 1) + acumulador] = arreglo[i -1];
+                            arreglo[i -1] = aux;
+                            ban = true;
+                        }
+                        i++;
+                    }
+
+                }
+                acumulador = acumulador / 2;
+            }
+            return arreglo;
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                longitud = Convert.ToInt32(txtBoxElementos.Text);
+                arreglo = new int[longitud];
+                for (int i = 0; i < longitud - 1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("llenar correctamente");
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)// llenar random
+        {
+            try
+            {
+                
+                int j = 0;
+                while (j < arreglo.Length)
+                {
+                    num = rand.Next(0, 200);
+                    dataGridView1[0, j].Value = num ;
+                    arreglo[j] = num;
+                    j++;
+                }
+                MessageBox.Show("arreglo lleno");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("llenar correctamente");
+            }
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            
+            
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("", "");
+            int[] arreglo2 = new int[longitud];
+               arreglo2= shellSort();
+            for (int i = 0; i < arreglo2.Length; i++)
+            {
+                dataGridView1[1, i].Value = arreglo2[i];
+            }
+            
+        }
+    }
+}
